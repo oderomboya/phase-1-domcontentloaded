bc4f22e212823bf59170131ecbe11e654f3a5e82
oderomboya <davismboya3@gmail.com>
2023-03-26 22:40:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+    const cool = document.querySelector('p');
+    cool.textContent = 'This is really cool!';
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
